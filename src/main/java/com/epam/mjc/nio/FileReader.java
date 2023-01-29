8aac177a9453d7aba15860117bcd09a6900edd8d
sadyrin <sadyrin@gmail.com>
2023-01-29 21:36:06 +0200

    done task stage1-module7-nio-task1

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,46 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class FileReader {
+    private String name;
+    private Integer age;
+    private String email;
+    private Long phone;
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        Path path;
+        try {
+            path = Paths.get(file.getAbsolutePath());
+
+            Stream<String> lines = Files.lines(path);
+    
+            String data = lines.collect(Collectors.joining("\n"));
+
+            lines.close();
+
+            String[] parts = data.split("\n");
+
+            for(String s : parts){
+				if(s.contains("Name:")) name = s.split(": ")[1];
+				if(s.contains("Age:")) age = Integer.parseInt(s.split(": ")[1]);
+				if(s.contains("Email:")) email = s.split(": ")[1];
+				if(s.contains("Phone:")) phone = Long.parseLong(s.split(": ")[1]);
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    
+        return new Profile(name, age, email, phone);
     }
 }
